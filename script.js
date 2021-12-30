ef6cdb59ef9d9300e4700176930c4f43b1a1cd02
= <=>
2021-12-30 21:18:56 +0530

    till rotation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
-import typeBasedArray from './cleanData.js';
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
+import typeBasedArray from "./cleanData.js";
 
-const gui = new dat.GUI();
+let gui = new dat.GUI();
 
 // console.log(typeBasedArray)
 
@@ -9,15 +9,26 @@ const gui = new dat.GUI();
 //     console.log(object)
 // })
 
-// const fieldTexture = new THREE.TextureLoader()
-// fieldTexture.load(  );
+const textureLoader = new THREE.TextureLoader();
+const fieldTexture = textureLoader.load("./ground.jpg");
+
+const bgTexture = textureLoader.load([
+  "./resources/posx.jpg",
+  "./resources/negx.jpg",
+  "./resources/posy.jpg",
+  "./resources/negy.jpg",
+  "./resources/posz.jpg",
+  "./resources/negz.jpg",
+]);
 
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 const scene = new THREE.Scene();
+
+scene.map = bgTexture;
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
 scene.add(ambientLight);
@@ -26,18 +37,18 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 directionalLight.position.set(200, 500, 300);
 scene.add(directionalLight);
 
-
 //adding field
 
 const fieldGeometry = new THREE.PlaneGeometry(50, 50);
-const fieldMaterial = new THREE.MeshStandardMaterial({ color: new THREE.Color('grey'), side: THREE.DoubleSide });
+const fieldMaterial = new THREE.MeshStandardMaterial({
+  color: new THREE.Color("grey"),
+  side: THREE.DoubleSide,
+});
 const field = new THREE.Mesh(fieldGeometry, fieldMaterial);
-field.rotation.x = -Math.PI/2;
-field.position.y = -0.01
+field.rotation.x = -Math.PI / 2;
+field.position.y = -0.01;
+// fieldMaterial.map = fieldTexture;
 scene.add(field);
-
-// field.rotation.x = 2
-// field.rotation.y = 2
 
 // DEBUG PANEL FOR FIELD
 
@@ -48,76 +59,139 @@ scene.add(field);
 
 // scene.add(fieldFolder)
 
-
 const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    1000
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
 );
 scene.add(camera);
 
-camera.position.x = -10;
-camera.position.y = 10;
+camera.position.x = 4;
+camera.position.y = 7;
 
-camera.position.z = 5;
-//making shapes 
+camera.position.z = 23;
+//making shapes
 
-let colors = ['red', 'green', 'blue', 'yellow','purple', 'orange', 'pink', 'cyan', 'grey']
+let colors = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "purple",
+  "orange",
+  "pink",
+  "cyan",
+  "grey",
+];
+
+gui.add(camera.position, "x");
+gui.add(camera.position, "y");
+gui.add(camera.position, "z");
+
+// making towers
+
 let makeCube = (typeBasedArray) => {
+  let z = 1 / 2;
+  let x = 1 / 2;
 
-    let z = 1 / 2;
-    let x = 1 / 2;
-    
-    for (let i = 0; i < typeBasedArray.length; i++) {
-        let currentObject = typeBasedArray[i];
-        for (let j = 0; j < currentObject.values.length; j++) {
-            const Cgeometry = new THREE.BoxGeometry(0.5, currentObject.values[j].height, 0.5);
-            const Cmaterial = new THREE.MeshStandardMaterial({ color: colors[i] });
-            const cube = new THREE.Mesh(Cgeometry, Cmaterial);
-            cube.position.x = x
-            
-            cube.position.y = currentObject.values[j].height/2;
-            cube.position.z = z
-            console.log(currentObject)
-            
-            scene.add(cube);
-            x++
-        }
-        x = 1/2;
-        z++
+  for (let i = 0; i < typeBasedArray.length; i++) {
+    let currentObject = typeBasedArray[i];
+    for (let j = 0; j < currentObject.values.length; j++) {
+      const Cgeometry = new THREE.BoxGeometry(
+        0.5,
+        currentObject.values[j].height,
+        0.5
+      );
+      const Cmaterial = new THREE.MeshStandardMaterial({ color: colors[i] });
+      const cube = new THREE.Mesh(Cgeometry, Cmaterial);
+      cube.position.x = x;
+
+      cube.position.y = currentObject.values[j].height / 2;
+      cube.position.z = z;
+      //   console.log(currentObject);
+
+      scene.add(cube);
+      x++;
     }
+    x = 1 / 2;
+    z++;
+  }
+};
+
+makeCube(typeBasedArray);
+
+//================================
+// -10, 10, 5
+
+let z = 23;
+let x = 4;
+let y = 10;
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == "38") {
+    // up arrow
+    z = z - 0.1;
+    camera.position.z = z;
+  } else if (e.keyCode == "40") {
+    // down arrow
+    z = z + 0.1;
+    camera.position.z = z;
+  } else if (e.keyCode == "37") {
+    // left arrow
+    x = x - 0.1;
+    camera.position.x = x;
+  } else if (e.keyCode == "39") {
+    // right arrow
+    x = x + 0.1;
+    camera.position.x = x;
+  }
 }
 
-makeCube(typeBasedArray)
-setTimeout(() => {
+document.addEventListener("keydown", (e) => {
+  //   mouseX = e.clientX;
+  //   console.log(mouseX);
+  checkKey(e);
+});
 
-}, 1000)
+let mouseX = 0;
+let mouseY = 0;
 
+let mouseMovement = (e) => {
+  mouseX = ((e.clientX - 0) / 1350) * 2 - 1;
+  mouseY = ((e.clientY - 0) / 620) * 2 - 1;
+  // console.log(mouseX, mouseY);
+};
 
+document.addEventListener("mousemove", mouseMovement);
+
+//==========================================
 
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
+  canvas: canvas,
 });
 
 renderer.setSize(sizes.width, sizes.height);
 
 const clock = new THREE.Clock();
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime();
 
-    controls.update()
-    renderer.render(scene, camera);
+  camera.rotation.x = mouseX;
+  camera.rotation.y = -mouseY;
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
+  // controls.update();
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
 };
 
 tick();
-
-
-
